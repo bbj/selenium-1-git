544fd86e61a092946480e81ea6ec4dfc7c444f78
Bruno Jullien <bruno.jullien@gmail.com>
2012-05-07 10:44:24 -0700

    first commit

---
+++ src/main/java/com/bbj/Selenium2Example.java
@@ -0,0 +1,9 @@
+package com.bbj;
+
+public class Selenium2Example  {
+    public static void main(String[] args) {
+    	
+    	//See the associated test class: Selenium2ExampleTest
+    	System.out.println("See the associated test class: Selenium2ExampleTest");
+    }
+}
